f27f9c0b2052a8124a0600749d8c471be6f02a14
a1dnan <aaliladnane@gmail.com>
2023-04-06 00:02:49 +0000

    Update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { CustomHttpResponse } from './interfaces/custom-http-response';
+import { AppState } from './interfaces/app-state';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from './service/note.service';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
+import { DataState } from './enums/datastate';
+import { Level } from './enums/level';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'notes-ui';
+export class AppComponent implements OnInit{
+
+  appState$ = new Observable<AppState<CustomHttpResponse>>;
+  readonly Level = Level;
+  readonly DataState = DataState;
+
+
+
+  constructor(private noteService: NoteService){}
+
+  ngOnInit(): void {
+
+    this.appState$ = this.noteService.notes$.pipe(
+      map(response =>{
+        return {dataState: DataState.LOADED, data: response}
+      }),
+      startWith( {dataState: DataState.LOADING} ),
+      catchError((err: string) => {
+        return of({ dataState: DataState.ERROR, error: err })
+      })
+    );
+    
+  }
+
+  
+  
 }
--- src/app/service/note.service.ts
+++ src/app/service/note.service.ts
@@ -1,10 +1,68 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { CustomHttpResponse } from '../interfaces/custom-http-response';
+import { Note } from '../interfaces/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
+  private readonly server = 'http://localhost:8080';
+
   constructor(private http: HttpClient) { }
+
+  // reactive approach
+  notes$ = <Observable<CustomHttpResponse>>this.http
+  .get<CustomHttpResponse>(`${this.server}/note`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  save$ = (note: Note) => <Observable<CustomHttpResponse>>this.http
+  .post<CustomHttpResponse>(`${this.server}/note`, note)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  update$ = (note: Note) => <Observable<CustomHttpResponse>>this.http
+  .put<CustomHttpResponse>(`${this.server}/note`, note)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  delete$ = (noteId: number) => <Observable<CustomHttpResponse>>this.http
+  .delete<CustomHttpResponse>(`${this.server}/note/${noteId}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    console.log(err);
+    let errorMessage : string;
+    if(err.error instanceof ErrorEvent){
+
+      errorMessage = `A client error occurred - ${err.error.message}`
+      
+    }else{
+
+      if(err.error.reason){
+
+        errorMessage = `${err.error.reason} - Error code ${err.status}`
+
+      }else{
+
+        errorMessage = `An error occurred - Error code ${err.status}`
+
+      }
+    }
+  
+    return throwError(errorMessage);
+    //return throwError(() => errorMessage);
+  }
 }
